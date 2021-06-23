f82ff849e47f571b26cf956343276d4d3eed3e33
S_ghanbary98 <67980207+Sherv1360@users.noreply.github.com>
2021-06-23 12:52:29 +0100

    using control flows(while loop integrated)

--- Movie_Ratings.py
+++ Movie_Ratings.py
@@ -1,18 +1,25 @@
 # Movie Ratings!
 
+while True:
+    rating_choice = input("What rating would you like to know more about? \n Options include: \n universal \n pg \n 12 \n 15 \n 18 \n\n")
+    if rating_choice == "universal":
+        print("\neveryone can watch!")
+
+    elif rating_choice == "pg":
+        print("\nGeneral viewing, but some scenes may be unsuitable for young children!")
+
+    elif rating_choice == "12":
+        print("\nFilms classified 12A and video works classified 12 contain material that is not generally suitable for children aged under 12. No one younger than 12 may see a 12A film in a cinema unless accompanied by an adult.")
+
+    elif rating_choice == "15":
+        print("\nNo one younger than 15 may see a 15 film in a cinema!")
+
+    elif rating_choice == "18":
+        print("\nNo one younger than 18 may see an 18 film in a cinema!")
+
+    elif rating_choice == "exit":
+        break
+    else:
+        print("Invalid option, Choose another")
 
 
-rating_choice = input("What rating would you like to know more about? \n\n Options include: \n universal \n pg \n 12 \n 15 \n 18 \n\n")
-
-if rating_choice == "universal":
-    print("everyone can watch!")
-elif rating_choice == "pg":
-    print("General viewing, but some scenes may be unsuitable for young children!")
-elif rating_choice == "12":
-    print("Films classified 12A and video works classified 12 contain material that is not generally suitable for children aged under 12. No one younger than 12 may see a 12A film in a cinema unless accompanied by an adult.")
-elif rating_choice == "15":
-    print("No one younger than 15 may see a 15 film in a cinema!")
-elif rating_choice == "18":
-    print("No one younger than 18 may see an 18 film in a cinema!")
-else:
-    print("invalid option")
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Movie Ratings!
 
-## In this project a user is able to choose from an option of movie ratings and receive back a description of the rating. This is done using an `if statement` and an `equals condition`
+### In this project a user is able to choose from an option of movie ratings and receive back a description of the rating. This is done using an `if statement` and an `equals condition` along with a `while loop` set to true.
