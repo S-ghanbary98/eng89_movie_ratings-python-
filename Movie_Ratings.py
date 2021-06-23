384ba5987d5555f0a775faa03b729905eae93b15
S_ghanbary98 <67980207+Sherv1360@users.noreply.github.com>
2021-06-23 12:29:13 +0100

    movie rating first commit

---
+++ Movie_Ratings.py
@@ -0,0 +1,18 @@
+# Movie Ratings!
+
+
+
+rating_choice = input("What rating would you like to know more about? \n\n Options include: \n universal \n pg \n 12 \n 15 \n 18 \n\n")
+
+if rating_choice == "universal":
+    print("everyone can watch!")
+elif rating_choice == "pg":
+    print("General viewing, but some scenes may be unsuitable for young children!")
+elif rating_choice == "12":
+    print("Films classified 12A and video works classified 12 contain material that is not generally suitable for children aged under 12. No one younger than 12 may see a 12A film in a cinema unless accompanied by an adult.")
+elif rating_choice == "15":
+    print("No one younger than 15 may see a 15 film in a cinema!")
+elif rating_choice == "18":
+    print("No one younger than 18 may see an 18 film in a cinema!")
+else:
+    print("invalid option")
